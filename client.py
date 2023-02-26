19186787ef202b10c05c3e693212bdd10016e42f
zaiyingzhao <zaiyingzhao@g.ecc.u-tokyo.ac.jp>
2023-02-26 20:52:19 +0900

    bug fix

--- client.py
+++ client.py
@@ -1,32 +1,8 @@
 import socket
 
-target_host = ""
-target_port = 50000
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((target_host, target_port))
-while True:
-    while True:
-        print("waiting for his response...")
-        rec = client.recv(1024)
-        # fd = open("tiredness.txt", "wb")
-        # fd.write(rec)
-        # fd.close()
-        print(rec)
-        if len(rec) == 0:
-            client.close()
-            break
-    # while True:
-    #     file = input("textfile: ")
-    #     try:
-    #         fd = open(file, "rb")
-    #     except FileNotFoundError:
-    #         print("can't open this file")
-    #         print("please try it again")
-    #         print("")
-    #     else:
-    #         text = fd.read()
-    #         fd.close()
-    #         break
-    # client.send(text)
-    # print("succeed")
-    # print("")
+tiredness = 1
+tiredness_b = tiredness.to_bytes(4, "big")
+print(tiredness_b)
+c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+c.connect(("", 51300))
+c.send(tiredness_b)
--- server.py
+++ server.py
@@ -1,30 +1,17 @@
 import socket
 
-bind_host = ""
-bind_port = 50000
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((bind_host, bind_port))
-server.listen(5)
-print("host: " + bind_host)
-print("port: " + str(bind_port))
-
-tiredness = 1
-
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("", 51300))
+s.listen(1)
 while True:
-    client, addr = server.accept()
-    # print("from:" + addr[0] + " " + str(addr[1]))
-    # fd = open("tiredness.txt", "rb")
-    # text = fd.read()
-    # fd.close()
-    server.send(tiredness)
-    # while True:
-    # print("waiting for his response...")
-    # rec = client.recv(1024)
-    # fd = open("tiredness.txt", "wb")
-    # fd.write(rec)
-    # fd.close()
-    # print(rec.decode("utf-8"))
-    # if len(rec) == 0:
-    #     client.close()
-    #     break
+    clientsocket, address = s.accept()
+    print("connection established!")
+    while True:
+        data_b = clientsocket.recv(1024)
+        if not data_b:
+            break
+        data_int = int.from_bytes(data_b, "big")
+        print(data_int)
+        print(data_int + 1)
+    clientsocket.close()
+s.close()
