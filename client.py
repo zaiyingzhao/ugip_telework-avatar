1af6504f17e51938b5e41d71f7b793cd6305cd79
zaiyingzhao <zaiyingzhao@g.ecc.u-tokyo.ac.jp>
2023-02-26 17:49:37 +0900

    for data transmission

--- client.py
+++ client.py
@@ -1,22 +1,32 @@
 import socket
 
-target_host = "(サーバ側のIPアドレス)"
+target_host = ""
 target_port = 50000
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
 while True:
     while True:
-        file = input("textfile: ")
-        try:
-            fd = open(file, "rb")
-        except FileNotFoundError:
-            print("can't open this file")
-            print("please try it again")
-            print("")
-        else:
-            text = fd.read()
-            fd.close()
+        print("waiting for his response...")
+        rec = client.recv(1024)
+        # fd = open("tiredness.txt", "wb")
+        # fd.write(rec)
+        # fd.close()
+        print(rec)
+        if len(rec) == 0:
+            client.close()
             break
-    client.send(text)
-    print("succeed")
-    print("")
+    # while True:
+    #     file = input("textfile: ")
+    #     try:
+    #         fd = open(file, "rb")
+    #     except FileNotFoundError:
+    #         print("can't open this file")
+    #         print("please try it again")
+    #         print("")
+    #     else:
+    #         text = fd.read()
+    #         fd.close()
+    #         break
+    # client.send(text)
+    # print("succeed")
+    # print("")
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import socket
 
-bind_host = "0.0.0.0"
+bind_host = ""
 bind_port = 50000
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,16 +9,22 @@ server.listen(5)
 print("host: " + bind_host)
 print("port: " + str(bind_port))
 
+tiredness = 1
+
 while True:
     client, addr = server.accept()
-    print("from:" + addr[0] + " " + str(addr[1]))
-    while True:
-        print("waiting for his response...")
-        rec = client.recv(1024)
-        fd = open("get.txt", "wb")
-        fd.write(rec)
-        fd.close()
-        print(rec.decode("utf-8"))
-        if len(rec) == 0:
-            client.close()
-            break
+    # print("from:" + addr[0] + " " + str(addr[1]))
+    # fd = open("tiredness.txt", "rb")
+    # text = fd.read()
+    # fd.close()
+    server.send(tiredness)
+    # while True:
+    # print("waiting for his response...")
+    # rec = client.recv(1024)
+    # fd = open("tiredness.txt", "wb")
+    # fd.write(rec)
+    # fd.close()
+    # print(rec.decode("utf-8"))
+    # if len(rec) == 0:
+    #     client.close()
+    #     break
